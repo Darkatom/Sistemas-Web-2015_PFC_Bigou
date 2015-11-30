e1b821300cec5df38dd4aa6112cf645a04d0ef78
Darkatom <darkatomish@gmail.com>
2015-12-01 00:30:49 +0100

    v.4.0.

---
+++ v.4.0/albums.php
@@ -0,0 +1,40 @@
+﻿<?php
+	include_once './business_logic/functions/database_logic.php';
+	include './business_logic/functions/menu_logic.php';
+	session_start();
+	if (!isset($_SESSION['nick']))
+		header('Location: main.php');
+	$nick = $_SESSION['nick'];
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Bigou - Mis Álbumes</title>       
+        <link href="style/bigou_style.css" rel="stylesheet" type="text/css" />
+        <!--<link rel='stylesheet' type='text/css' media='only screen and (max-width: 480px)' href='estilos/smartphone.css'/>-->
+	</head>  
+    <body>
+	<body>
+		<div class="Canvas">
+			<?php echo menuHeader(true, $nick, $_SESSION['role']); ?>
+			<div class="Display">
+				<?php
+					$userAlbums = getAlbums($nick); 
+					foreach($userAlbums as $album ) {				
+						echo "<div class='Album'>
+								<img src='".$album['cover']."'/>
+								<p>".$album['name']."</p>
+								<a href=''><button class='Basic Fancy' name='photos' onClick=''>Ver</button></a><!--
+								--><button class='Basic Fancy' name='delete' onClick=''>&#10008</button>
+							  </div>";	
+							  // AJAX para borrar Álbumes
+					}
+				?> 
+			</div>    
+			<br/><br/>   
+		</div>
+	</body>
+</html>
---
+++ v.4.0/business_logic/functions/database_logic.php
@@ -0,0 +1,121 @@
+<?php
+	
+	/*////////// UserID //////////
+	Olatz	=	u623652398_;	//
+	José	=	u837753965_;	//
+	////////////////////////////*/
+	
+	function makeQuery($sql) {	
+		$server = 'mysql.hostinger.es';
+		$database = 'u623652398_bigou';
+		$user = 'u623652398_admin';
+		$password = 'FU6k1nGM4s7e5S';
+	
+		$conexion = new mysqli($server, $user, $password, $database);	
+		if ($conexion->connect_error) {		
+			return false;
+		}
+		$query = $conexion->query($sql);
+		$conexion->close();
+		return $query;
+	}
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+	function addUser($nick, $password, $email, $name, $surname, $age, $gender) {
+		return makeQuery("INSERT INTO user (nick, password, email, name, surname, age, gender)
+							   VALUES ('{$nick}','{$password}', '{$email}',
+									   '{$name}', '{$surname}', '{$age}', '{$gender}')");	
+	
+	}
+	
+	function addConnection($nick, $email, $ip) { 
+		return makeQuery("INSERT INTO connection (nick, email, ip) 
+						  VALUES ('{$nick}', '{$email}', '{$ip}')");
+	}
+	
+	function addAction($nick, $email, $ip, $action) {
+		// 'register', 'logged_in', 'logout', 'new_photo', 'new_album'
+		// 'delete_photo', 'delete_album', 'change_password', 'deleted_account'		
+		
+		if (strcmp($action, 'register') != 0 and 
+			strcmp($action, 'logged_in') != 0 and strcmp($action, 'logout') != 0 and 
+			strcmp($action, 'new_photo') != 0 and strcmp($action, 'new_album') != 0 and 
+			strcmp($action, 'delete_photo') != 0 and strcmp($action, 'delete_album') != 0 and 
+			strcmp($action, 'change_password') != 0 and strcmp($action, 'deleted_account') != 0 )
+				return false;
+		
+		return makeQuery("INSERT INTO action (nick, email, ip, action) VALUES ('{$nick}', '{$email}', '{$ip}', '{$action}')");	
+	}
+
+	function addAlbum($nick, $albumName, $access, $coverPath) {
+		if (strcmp($coverPath, "DEFAULT") == 0)
+			return makeQuery("INSERT INTO album (nick, name, access, cover) VALUES ('{$nick}', '{$albumName}', '{$access}', DEFAULT)");
+		
+		return makeQuery("INSERT INTO album (nick, name, access, cover) VALUES ('{$nick}', '{$albumName}', '{$access}', '{$coverPath}')");
+	}
+	
+	function addPhoto($nick, $path, $albumName) {
+		return makeQuery("INSERT INTO photo (nick, path, album) VALUES ('{$nick}', '{$path}', '{$albumName}')");
+	}
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
+	
+	function getPassword($nick) {
+		return mysqli_fetch_assoc(makeQuery("SELECT password FROM user WHERE nick = '{$nick}'"))['password'];		
+	}
+	
+	function getEmail($nick) {
+		return mysqli_fetch_assoc(makeQuery("SELECT email FROM user WHERE nick='{$nick}'"))['email'];
+	}
+	
+	function getRole($nick) {
+		return mysqli_fetch_assoc(makeQuery("SELECT role FROM user WHERE nick='{$nick}'"))['role'];
+	}
+	
+	function getAlbums($nick) {
+		return makeQuery("SELECT * FROM album WHERE nick='{$nick}'");
+	}
+	
+	function setPassword($nick, $password) {
+		return makeQuery("UPDATE user SET password='{$password}' WHERE nick='{$nick}'");
+	}
+	
+	function setAvatar($nick, $avatar_path) {
+		return makeQuery("UPDATE user SET avatar='{$avatar_path}' WHERE nick='{$nick}'");
+	}
+	
+	function setAlbumCover($nick, $albumName, $albumCoverPath) {
+		if (strcmp($albumCoverPath, "DEFAULT") == 0)
+			return makeQuery("UPDATE album SET cover=DEFAULT WHERE nick='{$nick}' AND name='{$albumName}''");
+			
+		return makeQuery("UPDATE album SET cover='{$albumCoverPath}' WHERE nick='{$nick}' AND name='{$albumName}'");
+	}
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	function isAlbum($nick, $albumName) {
+		return mysqli_num_rows(makeQuery("SELECT * FROM album WHERE nick='$nick' AND name='$albumName'")) > 0;
+	}
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	function get_client_ip() {
+		$ipaddress = '';
+		if (getenv('HTTP_CLIENT_IP'))
+			$ipaddress = getenv('HTTP_CLIENT_IP');
+		else if(getenv('HTTP_X_FORWARDED_FOR'))
+			$ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+		else if(getenv('HTTP_X_FORWARDED'))
+			$ipaddress = getenv('HTTP_X_FORWARDED');
+		else if(getenv('HTTP_FORWARDED_FOR'))
+			$ipaddress = getenv('HTTP_FORWARDED_FOR');
+		else if(getenv('HTTP_FORWARDED'))
+		   $ipaddress = getenv('HTTP_FORWARDED');
+		else if(getenv('REMOTE_ADDR'))
+			$ipaddress = getenv('REMOTE_ADDR');
+		else
+			$ipaddress = 'UNKNOWN';
+		return $ipaddress;
+	}
+?> 
